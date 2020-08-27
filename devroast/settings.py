502c4ed6e62b045bbdc9487df2656be6af9f4cc0
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-08-26 19:51:16 -0700

    Fixed data persistence, updated db env vars.

--- devroast/settings.py
+++ devroast/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'devroast.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASS"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("POSTGRES_NAME"),
+        'USER': os.getenv("POSTGRES_USER"),
+        'PASSWORD': os.getenv("POSTGRES_PASS"),
+        'HOST': os.getenv("POSTGRES_HOST"),
+        'PORT': os.getenv("POSTGRES_PORT"),
     }
 }
 
