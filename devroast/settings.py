402c1a48396a47170a832d76e65292f513f6796e
crg253 <crg253@protonmail.com>
2020-09-07 19:30:55 -0700

    Many changes to docker setup

--- devroast/settings.py
+++ devroast/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'devroast.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', 
-        'NAME': os.getenv("POSTGRES_NAME"),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv("POSTGRES_DB"),
         'USER': os.getenv("POSTGRES_USER"),
-        'PASSWORD': os.getenv("POSTGRES_PASS"),
+        'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
         'HOST': os.getenv("POSTGRES_HOST"),
         'PORT': os.getenv("POSTGRES_PORT"),
     }
