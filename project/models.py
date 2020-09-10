56b38a5a19b3e73694386f85eb52d842c2c56ce1
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-09-10 12:24:46 -0700

    Fixed typos in Project model.

--- project/models.py
+++ project/models.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User
 class Project(models.Model):
 
 	# The author, related to auth_user via many-to-one
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 	# The name of the website to be reviewed
     title = models.CharField(max_length=250, help_text="The name of your project.")
 	# The git repository for the website
     repo_url = models.CharField(max_length=250, help_text="URL to your project's source code.")
 	# The URL where a live version can be viewed
-    hosted_url = models.CharField(mxa_length=250, help_text="Your website's URL.")
+    hosted_url = models.CharField(max_length=250, help_text="Your website's URL.")
 	# A summary of the project
     description = models.TextField(help_text="A brief description of your website.")
 
