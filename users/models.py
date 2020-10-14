32f76ae93b9b11794eff9b77180e82efffadfcfc
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-14 16:09:12 -0700

    Started work on user profile model.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# Model for user profile info
+class Profile(models.Model):
+
+    # Define pronoun options for users
+    PRONOUNS = (
+        ("He/Him", "Male"),
+        ("She/Her", "Female"),
+        ("They/Them", "NB"),
+    )
+
+    user = models.OneToOneField(User)
+    username = models.CharField(length=255, help_text="Your username", unique=True)
+    role = models.CharField(length=255, help_text="Web Developer, Data Engineer, etc")
+    location = models.CharField(length=255, help_text="What city your located in")
+    pronoun = models.CharField(length=6, choices=PRONOUNS)
+
+    def __str__(self):
+
+        return self.username
