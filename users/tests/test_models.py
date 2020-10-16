eee52401618155946690335dc88f79d2c511b457
D-Bits <Dana.Lockwood2@seattlecolleges.edu>
2020-10-16 16:42:49 -0700

    Cleaned up Profile model unit tests.

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -6,7 +6,7 @@ from users.models import Profile
 
 class UserModelTests(TestCase):
 
-    def test_user_creation(self):
+    def setUp(self):
 
         # Create a test user 
         test_user = User.objects.create_user(
@@ -18,6 +18,16 @@ class UserModelTests(TestCase):
             password="lagavulin16",
         )
 
+        test_profile = Profile.objects.create(
+            user= User.objects.get(id=1),
+            role="Director, Pawnee Parks and Recreation.",
+            location="Pawnee, IN",
+            pronouns="He/Him"
+        )
+
+    def test_user_creation(self):
+
+        test_user = User.objects.get(id=1)
         self.assertEqual(test_user.username, "rswanson")
         self.assertEqual(test_user.email, "rswanson@pawnee.gov")
         self.assertEqual(test_user.first_name, "Ron")
@@ -26,14 +36,8 @@ class UserModelTests(TestCase):
     # Test that a default profile is automatically created after user registration
     def test_profile_creation(self):
 
-        test_profile = Profile.objects.create(
-            user=1,
-            role="Director, Pawnee Parks and Recreation.",
-            location="Pawnee, IN",
-            pronoun="He/Him"
-        )
-
-        self.assertEqual(test_profile.user, 1)
+        test_profile = Profile.objects.get(user=User.objects.get(username="rswanson"))
+        self.assertEqual(test_profile.user, User.objects.get(username="rswanson"))
         self.assertEqual(test_profile.role, "Director, Pawnee Parks and Recreation.")
         self.assertEqual(test_profile.location, "Pawnee, IN")
-        self.assertEqual(test_profile.location, "He/Him")
+        self.assertEqual(test_profile.pronouns, "He/Him")
