bfd15f1c812eef8686ba09a272c69f91cdd1a1ec
asgray <antgray89@gmail.com>
2020-11-01 21:20:29 -0800

    form works, backend problems

--- frontend/react-frontend/src/components/RegistrationForm.jsx
+++ frontend/react-frontend/src/components/RegistrationForm.jsx
@@ -5,11 +5,13 @@ const RegistrationForm = () => {
     const [newUser, setNewUser] = useState({})
 
     const postRequest = async () => {
-        const response = await fetch('http://localhost:8000/api/auth/registration/', {
+        const dat =JSON.stringify(newUser)
+        console.log(dat)
+        const response = await fetch('http://localhost:8000/rest-auth/registration/', {
             method: 'POST', 
             mode: 'no-cors', 
             headers: {'Content-Type': 'application/json'}, 
-            body: JSON.stringify(newUser)
+            body: dat
         });
         console.log(response)
     }
@@ -24,19 +26,19 @@ const RegistrationForm = () => {
         <form onSubmit={registerUser}>
             <label htmlFor='user'>
             User Name
-            <input type='text' name='user' required={true} value={ newUser.name || '' } onChange={e => setNewUser({...newUser, name: e.target.value})}/>
+            <input type='text' name='username' value={ newUser.username || '' } onChange={e => setNewUser({...newUser, 'username': e.target.value})}/>
             </label>
             <label htmlFor='email'>
             Email
-            <input type='text' name='email' required={true} value={ newUser.email || '' } onChange={e => setNewUser({...newUser, email: e.target.value})}/>
+            <input type='text' name='email' value={ newUser.email || '' } onChange={e => setNewUser({...newUser, 'email': e.target.value})}/>
             </label>
             <label htmlFor='password1'>
             Password
-            <input type='text' name='password1' required={true} value={ newUser.password1 || '' } onChange={e => setNewUser({...newUser, password1: e.target.value})}/>
+            <input type='text' name='password1' value={ newUser.password1 || '' } onChange={e => setNewUser({...newUser, 'password1': e.target.value})}/>
             </label>
             <label htmlFor='password2'>
             Confirm Password
-            <input type='text' name='password2' required={true} value={ newUser.password2 || '' } onChange={e => setNewUser({...newUser, password2: e.target.value})}/>
+            <input type='text' name='password2' value={ newUser.password2 || '' } onChange={e => setNewUser({...newUser, 'password2': e.target.value})}/>
             </label>
             <br />
             <input type="submit" value="Submit" />
