80d1d177b808a21aee2225285012f78af61d07ac
asgray <antgray89@gmail.com>
2020-12-30 01:03:45 -0800

    alerts implemented

--- frontend/react-frontend/src/components/useInput.jsx
+++ frontend/react-frontend/src/components/useInput.jsx
@@ -2,6 +2,6 @@ import React, {useState} from "react";
 
 export const useInput = ({ type, label }) => {
     const [value, setValue] = useState("");
-    const input = <> <label> {label} <input value={value} onChange={e => setValue(e.target.value)} type={type} /> </label> </>;
+    const input = <label> {label} <input value={value} onChange={e => setValue(e.target.value)} type={type} /> </label>;
     return [value, input];
   }
