1d7d99c11b82025c3da6082a656c8ca2811d249e
Anthony <antgray89@gmail.com>
2021-03-05 18:06:10 -0800

    splitting login branch

--- frontend/react-frontend/src/services/logout.js
+++ frontend/react-frontend/src/services/logout.js
@@ -1,5 +1,5 @@
 export const logOut = (user, setUser) => {
-    setUser({...user, token: null, info: null})
     localStorage.removeItem("user_token")
     localStorage.removeItem("token_time")
+    setUser({...user, token: null, info: null})
 }
