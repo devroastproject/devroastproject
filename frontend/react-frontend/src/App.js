639af1a6a3f986099aaffae87712ef4f68da0e34
Anthony <antgray89@gmail.com>
2021-03-19 00:12:35 -0700

    debug persist login

--- frontend/react-frontend/src/App.js
+++ frontend/react-frontend/src/App.js
@@ -6,7 +6,7 @@ import UserContext from './context/UserContext';
 import Projects from "./components/Projects";
 import Profile from "./components/Profile";
 import { logOut } from "./services/logout";
-import { callApi } from "./services/api";
+import { callApi } from "./services/callAPI";
 import Login from "./components/Login";
 import Nav from "./components/Nav";
 
@@ -24,20 +24,27 @@ function App() {
     if (!user.token){ // check if token is in local storage
       let userToken = localStorage.getItem("user_token")
       let tokenTime = localStorage.getItem("token_time")
-      let timeDiff = 30  //                              <----------  time in minutes until token expires
-      let expired = (Date.now() - timeDiff*60000) > tokenTime
-      if (userToken && !expired){
-        setUser({...user, token: userToken}) 
-      } else {
-        logOut(user, setUser)
+      if (userToken && tokenTime) {
+        let timeDiff = 30  //                              <----------  time in minutes until token expires
+        let expired = (Date.now() - timeDiff*60000) > tokenTime
+        if (!expired){
+          setUser({...user, token: userToken}) 
+        }
+        else {
+          logOut(user, setUser)
+        }
       }
     }
+  },[user])
+
+  useEffect(() => {
     if (user.token && !user.info) {   // trigger api call if the token has been retrieved, but if the user has not been fetched
       (async () => {
         const userinfo = await callApi("users/me", "GET", null, user.token)
         setUser({...user, info: userinfo})
       })()
-    }}, [user]);              // trigger effect if user is modified
+    }
+   }, [user])              // trigger effect if user is modified
   
   // time out user messages
   useEffect(() => {
