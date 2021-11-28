f9d1ebdcc5ae57013aee920b72d531da104bdf82
Anthony <antgray89@gmail.com>
2021-11-27 20:48:34 -0800

    start tag branch

--- frontend/react-frontend/cypress/integration/Comment.spec.js
+++ frontend/react-frontend/cypress/integration/Comment.spec.js
@@ -113,33 +113,3 @@ describe('Comment CRUD not logged in', () => {
     })
 
 })
-
-// describe('Tag Tests', () => {
-//     beforeEach(() => {
-        
-//     })
-
-//     it('users with 3 or more comments can create a new tag', () => {
-//         throw new Error('stub')
-//     })
-
-//     it('users with 3 or more comments can edit a tag', () => {
-//         throw new Error('stub')
-//     })
-
-//     it('users with 3 or more comments can delete a tag', () => {
-//         throw new Error('stub')
-//     })
-
-//     it('a tag can be added to a comment', () => {
-//         throw new Error('stub')
-//     })
-
-//     it('a tag can be removed from a comment', () => {
-//         throw new Error('stub')
-//     })
-
-//     it('a tag added to a reply is added to the prompt', () => {
-//         throw new Error('stub')
-//     })
-// })
